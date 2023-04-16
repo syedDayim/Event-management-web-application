da9461310ed4595fb291bb6136d1d3678a08480e
Syed Dayim Shah <dayim1277@gmail.com>
2023-04-16 12:33:46 +0530

    Learn page complete

--- itvent/settings.py
+++ itvent/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [ 
+    BASE_DIR, 'static',
+]
