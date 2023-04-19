a4703f15fc62570abdca2542f84985a6803711d4
Syed Dayim Shah <dayim1277@gmail.com>
2023-04-19 16:02:12 +0530

    Added Redirect

--- events/views.py
+++ events/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .forms import EventRegistrationForm
+
 # Create your views here.
 def registrationSuccess(request):
     return render(request, 'events/success.html',{})
 
 
 def events(request):
-    form = EventRegistrationForm()
+    if request.method == 'POST':
+        form = EventRegistrationForm(request.POST)  
+        if form.is_valid():
+            return HttpResponseRedirect('sucess/')
+    else:
+        form = EventRegistrationForm()
+    # HttpResponseRedirect('/events/success')
     return render(request, 'events/events.html', {'form': form})
