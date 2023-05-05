04d8ac9fea7098af192d6ca66959ae1faf023a46
Syed Dayim Shah <dayim1277@gmail.com>
2023-05-05 16:50:58 +0530

    Fixed bugs

--- events/models.py
+++ events/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import RegexValidator
 
 class EventRegistration(models.Model):
     name = models.CharField(max_length=25)
     email = models.EmailField(max_length=100)
-    semester = models.IntegerField(validators=[MaxValueValidator(9), MinValueValidator(1)])
+    semester = models.IntegerField()
     enrolment_no = models.CharField(max_length=11)
     department = models.CharField(max_length=100)
